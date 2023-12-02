231d0b1f9ad20c1c786e9e43b11f3a43701355f8
Manu Espinosa <jose_el@tesch.edu.mx>
2023-12-02 13:31:53 -0600

    file mappings added

--- helpers/getFilePath.js
+++ helpers/getFilePath.js
@@ -2,7 +2,15 @@ export const getFilePath = (fileName) => {
     // Define a mapping of file names to file paths
     const fileMappings = {
       "SGI_G3_3.1_INSTRUM_DIDAC_PLANES_2015_2022": "./templates/SGI_G3_3.1_INSTRUM_DIDAC_PLANES_2015_2022.pdf",
-      "ACA-0907-I": "./templates/materias/1/ACA-0907/ACA-0907-I.pdf"
+
+      /**Instrumentaciones primer semestre */
+      "ACA-0907-I": "./templates/materias/1/ACA-0907/ACA-0907-I.pdf",
+      "ACC-0906-I": "./templates/materias/1/ACC-0906/ACC-0906-I.pdf",
+      "ACF-0901-I": "./templates/materias/1/ACF-0901/ACF-0901-I.pdf",
+      "AED-1285-I": "./templates/materias/1/AED-1285/AED-1285-I.pdf",
+      "AEF-1041-I": "./templates/materias/1/AEF-1041/AEF-1041-I.pdf",
+      "SCH-1024-I": "./templates/materias/1/SCH-1024/SCH-1024-I.pdf",
+
       // Add more file mappings as needed
     };
   
